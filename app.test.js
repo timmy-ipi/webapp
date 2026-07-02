7393e83b44837db58b07ddbe474fea448eed4656
timmy-ipi <timmy.bucheton@edu.igensia.com>
2026-07-02 11:25:24 +0200

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,1 +1,23 @@
-const { calcul } = require('./app');test('2 + 3 = 5', () => {expect(calcul(2, 3)).toBe(5);});
+const calcul = require('./app');
+
+describe('Fonction calcul', () => {
+  test('2 + 3 = 5', () => {
+    expect(calcul(2, 3)).toBe(5);
+  });
+
+  test('1 + 2 = 3', () => {
+    expect(calcul(1, 2)).toBe(3);
+  });
+
+  test('0 + 0 = 0', () => {
+    expect(calcul(0, 0)).toBe(0);
+  });
+
+  test('-1 + -2 = -3', () => {
+    expect(calcul(-1, -2)).toBe(-3);
+  });
+
+  test('5 + -2 = 3', () => {
+    expect(calcul(5, -2)).toBe(3);
+  });
+});
