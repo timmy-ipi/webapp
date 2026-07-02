cf4484c48997308120f201446dff08501085bb5b
timmy-ipi <timmy.bucheton@edu.igensia.com>
2026-07-02 12:36:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 
 // Sonde utilisée par Kubernetes : readiness / liveness
 app.get('/health', (req, res) => {
-  res.status(200).send('Timmy');
+  res.status(500).send('Timmy');
 });
 
 // On ne démarre le serveur que si le fichier est lancé directement.
